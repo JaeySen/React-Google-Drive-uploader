2e143089ce827524c830ab67b99d089bb776e207
JaeySen <longhientranjjj@gmail.com>
2023-08-30 16:28:53 +0700

    Add listFiles, currently CORS-ed

--- src/App.js
+++ src/App.js
@@ -1,156 +1,280 @@
-import axios from 'axios'; 
-import * as fs from 'fs';
-import React,{Component} from 'react'; 
-import { google } from 'googleapis';
-import readline from 'readline';
-class App extends Component { 
+import React, { useState, useEffect } from "react";
 
-	state = { 
+const App = () => {
+  const [config, setConfig] = useState({
+    // Initially, no file is selected
+    selectedFile: null,
+    TOKEN_PATH: "token.json",
+    SCOPES: ["https://www.googleapis.com/auth/drive"],
+  });
 
-	// Initially, no file is selected 
-  selectedFile: null,
-  TOKEN_PATH: 'token.json',
-  SCOPES:['https://www.googleapis.com/auth/drive.metadata.readonly']
-	}; 
-	
-	// On file select (from the pop up) 
-	onFileChange = event => { 
-	
-	// Update the state 
-	this.setState({ selectedFile: event.target.files[0] }); 
-	
-  }; 
-  
-	authorize = (credentials, callback) => {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-  
-    // Check if we have previously stored a token.
-    fs.readFile(this.TOKEN_PATH, (err, token) => {
-      if (err) return this.getAccessToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token));
-      callback(oAuth2Client);
-    });
-  }
+  // Replace with your API key, copied from https://console.cloud.google.com/apis/credentials
+  const apiKey = "AIzaSyCS16uoSkvAw8qWISlAPLYPNCC4__6qYps";
 
-  getAccessToken = (oAuth2Client, callback) => {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: this.SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error retrieving access token', err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(this.TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) return console.error(err);
-          console.log('Token stored to', this.TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-    });
-  }
-  
-  listFiles = (auth) => {
-    const drive = google.drive({version: 'v3', auth});
-    drive.files.list({
-      pageSize: 10,
-      fields: 'nextPageToken, files(id, name)',
-    }, (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const files = res.data.files;
-      if (files.length) {
-        console.log('Files:');
-        files.map((file) => {
-          console.log(`${file.name} (${file.id})`);
-        });
-      } else {
-        console.log('No files found.');
-      }
-    });
-  }
+  // Replace with your Client ID, copied from https://console.cloud.google.com/apis/credentials
+  const clientId =
+    "1051229922030-in1rp6cmj0n2ccf5b1p6ggtn54hj1sa6.apps.googleusercontent.com";
 
-	// On file upload (click the upload button) 
-	onFileUpload = () => { 
-    fs.readFile('credentials.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Google Drive API.
-      this.authorize(JSON.parse(content), this.listFiles);
-    });
-    
-	// Create an object of formData 
-	//const formData = new FormData(); 
-	
-	// Update the formData object 
-	// formData.append( 
-	// 	"myFile", 
-	// 	this.state.selectedFile, 
-	// 	this.state.selectedFile.name 
-	// ); 
-	
-	// Details of the uploaded file 
-	//console.log(this.state.selectedFile); 
-	
-	// Request made to the backend api 
-	// Send formData object 
-	//axios.post("api/uploadfile", formData); 
-	}; 
-	
-	// File content to be displayed after 
-	// file upload is complete 
-	fileData = () => { 
-	
-	if (this.state.selectedFile) { 
+  // Replace with your Project number, copied from https://console.cloud.google.com/iam-admin/settings
+  const projectNumber = "1051229922030";
+
+  // Replace with a mime type unique to your application
+  const mimeType = `application/my.app`;
+
+  let oauthToken, fileId;
+
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = "https://apis.google.com/js/api.js?onload=loadScripts";
+    script.type = "text/javascript";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    // const plaform = document.createElement('script');
+    // plaform.src = "https://apis.google.com/js/platform.js";
+    // document.body.appendChild(plaform);
+
+    const initScript = document.createElement("script");
+    initScript.innerHTML = `
+			function loadScripts() {
+				gapi.load('auth');
+				gapi.load('picker');
+				gapi.load('client', () => gapi.client.load('drive', 'v3'));
+			}
+		`;
+    initScript.type = "text/javascript";
+    document.body.appendChild(initScript);
+
+    // return () => {
+    // };
+  }, []);
+
+  // On file select (from the pop up)
+  const onFileChange = (event) => {
+    setConfig({ selectedFile: event.target.files[0] });
+  };
+
+  const listFiles = async (auth) => {
+
+    fetch("https://www.googleapis.com/drive/v3/files" + new URLSearchParams({
+		corpora: 'drive',
+		driveId: '1Dy8K6ka6m36m6nxfneJSvhYqzGVd0EMW',
+		q: 'name contains \'TESTED\'', 
+	}), {
+		// mode: "no-cors", //TODO: fix CORS on localhost:3000
+		method: "GET",
+		headers: new Headers({
+		  Authorization: "Bearer " + localStorage.getItem("token"),
+		//   'Access-Control-Allow-Origin': 'http://localhost:3000/',
+		  "Access-Control-Allow-Origin": "*",
+		  "Access-Control-Allow-Methods": "GET",
+		  "Access-Control-Allow-Headers": "Content-Type, Authorization"
+		})
 		
-		return ( 
-		<div> 
-			<h2>File Details:</h2> 
-			<p>File Name: {this.state.selectedFile.name}</p> 
-			<p>File Type: {this.state.selectedFile.type}</p> 
-			<p> 
-			Last Modified:{" "} 
-			{this.state.selectedFile.lastModifiedDate.toDateString()} 
-			</p> 
-		</div> 
-		); 
-	} else { 
-		return ( 
-		<div> 
-			<br /> 
-			<h4>Choose before Pressing the Upload button</h4> 
-		</div> 
-		); 
-	} 
-	}; 
-	
-	render() { 
-	
-	return ( 
-		<div> 
-			<h1> 
-			GeeksforGeeks 
-			</h1> 
-			<h3> 
-			File Upload using React! 
-			</h3> 
-			<div> 
-				<input type="file" onChange={this.onFileChange} /> 
-				<button onClick={this.onFileUpload}> 
-				Upload! 
-				</button> 
-			</div> 
-		{this.fileData()} 
-		</div> 
-	); 
-	} 
-} 
+	})
+      .then((result) => result.json(), (err) => console.log("error: ", err))
+    //   .then((value) => {
+    //     console.log("Uploaded. Result:\n" + JSON.stringify(value, null, 2));
+    //   });
+  };
 
-export default App; 
+  const upload = () => {
+    const metadata = {
+      name: config.selectedFile.name,
+      mimeType,
+      parents: ["1Dy8K6ka6m36m6nxfneJSvhYqzGVd0EMW"],
+    };
+    const form = new FormData();
+    form.append(
+      "metadata",
+      new Blob([JSON.stringify(metadata)], { type: "application/json" })
+    );
+    // form.append('file', JSON.stringify({ hello: 'world' }));
+    form.append("file", config.selectedFile);
+
+    fetch(
+      "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+      {
+        method: "POST",
+        headers: new Headers({
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        }),
+        body: form,
+      }
+    )
+      .then((result) => result.json())
+      .then((value) => {
+        console.log("Uploaded. Result:\n" + JSON.stringify(value, null, 2));
+        fileId = value.id;
+        document.getElementById("get").disabled = false;
+        document.getElementById("update").disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  // // File content to be displayed after
+  // // file upload is complete
+  const fileData = () => {
+    if (config.selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {config.selectedFile.name}</p>
+          <p>File Type: {config.selectedFile.type}</p>
+          <p>
+            Last Modified: {config.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
+
+  // From jsGoogleDrivDemo: https://github.com/RickMohr/jsGoogleDriveDemo
+
+  function authorize() {
+    if (!window.gapi.auth) return;
+    window.gapi.auth.authorize(
+      {
+        client_id: clientId,
+        scope: ["https://www.googleapis.com/auth/drive"],
+        plugin_name: "Web client 1",
+        immediate: false,
+      },
+      (result) => {
+        if (result.error) {
+          console.log(result.error);
+        } else {
+          oauthToken = result.access_token;
+          console.log(`Authorized. Token: ${oauthToken}`);
+          localStorage.setItem("token", oauthToken);
+          window.gapi.client
+            .init({
+              apiKey: apiKey,
+              // clientId and scope are optional if auth is not required.
+              clientId: clientId,
+              plugin_name: "Web client 1",
+              scope: "profile",
+            })
+            .then(function () {
+              // 3. Initialize and make the API request.
+              return window.gapi.client.request({
+                path: "/drive/v3/about",
+                method: "GET",
+                params: { fields: ["kind"] },
+              });
+            })
+            .then(
+              function (response) {
+                console.log(response.result);
+              },
+              function (reason) {
+                console.log("Error: " + reason.result.error.message);
+              }
+            );
+          // document.getElementById('upload').disabled = false;
+          // document.getElementById('pick').disabled = false;
+        }
+      }
+    );
+  }
+
+  // From https://stackoverflow.com/a/55095062/362702
+  // For upload we don't use gapi.client.drive.files.create() because it doesn't accept file content
+
+  const get = (fileId) => {
+    window.gapi.client.drive.files
+      .get({ fileId, alt: "media" })
+      .then((result) =>
+        console.log("Fetched. Result: " + JSON.stringify(result.result))
+      )
+      .catch((err) => console.error(err));
+  };
+
+  function update() {
+    const url =
+      "https://www.googleapis.com/upload/drive/v3/files/" +
+      fileId +
+      "?uploadType=media";
+    fetch(url, {
+      method: "PATCH",
+      headers: new Headers({
+        Authorization: "Bearer " + oauthToken,
+        "Content-type": mimeType,
+      }),
+      body: JSON.stringify({ hello: "universe" }),
+    })
+      .then((result) => result.json())
+      .then((value) => {
+        console.log("Updated. Result:\n" + JSON.stringify(value, null, 2));
+      })
+      .catch((err) => console.error(err));
+  }
+
+  // Modified from https://developers.google.com/picker/docs#example
+
+  // function createPicker() {
+  // 	const view = new google.picker.View(google.picker.ViewId.DOCS);
+  // 	view.setMimeTypes(mimeType);
+  // 	const picker = new google.picker.PickerBuilder()
+  // 		.enableFeature(google.picker.Feature.NAV_HIDDEN)
+  // 		.enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+  // 		.setAppId(projectNumber)
+  // 		.setOAuthToken(oauthToken)
+  // 		.addView(view)
+  // 		.addView(new google.picker.DocsUploadView())
+  // 		.setDeveloperKey(apiKey)
+  // 		.setCallback(pickerCallback)
+  // 		.build();
+  // 	picker.setVisible(true);
+
+  // 	function pickerCallback(data) {
+  // 		if (data.action == google.picker.Action.PICKED) {
+  // 			const fileId = data.docs[0].id;
+  // 			console.log('Picked. FileId: ' + fileId);
+  // 			get(fileId);
+  // 		}
+  // 	}
+  // }
+
+  return (
+    <div>
+      <h1>File Upload using React!</h1>
+      <h3>Authorize then upload</h3>
+      <div>
+        <button id="authorize" onClick={() => authorize()}>
+          Authorize
+        </button>
+        <input type="file" onChange={onFileChange} />
+        {/* <button onClick={onFileUpload} disabled> Upload! </button> */}
+        <button id="upload" onClick={() => upload()}>
+          Upload
+        </button>
+        <button
+          id="get"
+          onClick={(fileId) => get("1yueZ7odCTAKCxBy1AGWxckWiShcl2wOW")}
+        >
+          Get
+        </button>
+        <button id="list" onClick={() => listFiles()}>
+          List Files
+        </button>
+        <button id="update" onClick={() => update()} disabled>
+          Update
+        </button>
+        {/* <button id="pick" onClick={() => createPicker()} disabled>Pick</button> */}
+      </div>
+      {fileData()}
+    </div>
+  );
+};
+
+export default App;
